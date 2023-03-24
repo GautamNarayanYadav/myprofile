5a369c8ed3e5881c66b47f58eb61e7ae58974619
gautamyadav <gautamyadav@spliceglobal.com>
2023-03-24 16:41:02 +0530

    add now

--- resume/views.py
+++ resume/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# def hello(request):
+#     return HttpResponse("<h1>Welcome to Gautam Yadav</h1>")
+
+
 def hello(request):
-    return HttpResponse("<h1>Welcome to Gautam Yadav</h1>")
+    # render function takes argument  - request
+    # and return HTML as response
+    return render(request, "index.html")
